894f9d920b181321d4f2b1e14827ee0a79196b02
Ross L. Owen <ross.lloyd.owen@gmail.com>
2024-06-08 08:29:57 -0600

    week3: fraction using encapsulation

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,67 @@
+﻿namespace Learning03;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = _numerator == 0 ? 0 : 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0 && numerator != 0)
+        {
+            throw new ArgumentException("denominator cannot be zero");
+        }
+
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public int Numerator
+    {
+        get => _numerator;
+        set
+        {
+            _numerator = value;
+            if (value == 0)
+            {
+                _numerator = 0;
+            }
+        }
+    }
+
+    public int Denominator
+    {
+        get => _denominator;
+        set
+        {
+            if (value == 0)
+            {
+                throw new ArgumentException("denominator cannot be zero");
+            }
+
+            _denominator = value;
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,18 @@
-using System;
+namespace Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Display(new Fraction());
+        Display(new Fraction(5));
+        Display(new Fraction(3, 4));
+        Display(new Fraction(1, 3));
+    }
+
+    private static void Display(Fraction fraction)
+    {
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
